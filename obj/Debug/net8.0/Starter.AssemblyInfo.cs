b1a9b28e632dc401656df804ed248a368328bd95
Richard Lirio <richardlirio@hotmail.com>
2024-08-06 15:37:29 -0300

    Acrescentando switch case para as opções do menu

--- obj/Debug/net8.0/Starter.AssemblyInfo.cs
+++ obj/Debug/net8.0/Starter.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Starter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c744bce4c6f25893d9d745fbcc5673d966a40fcd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9927a41c3f3b673acba271015cbcdfded0759a02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Starter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Starter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
